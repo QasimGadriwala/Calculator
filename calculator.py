c249d62c9d0c70f71009f8d76dabdb5fdd1426c6
QasimGadriwala <qasimgadriwala@gmail.com>
2024-08-07 22:46:39 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+while True:
+    function = input('''What would you like to do?
+    1. Add
+    2. Subtract
+    3. Multiply
+    4. Divide
+    5. Quit ''').lower()
+    if function == "quit":
+        break
+    numbers = input("Please enter the numbers you would like to use separated by a space: ")
+    num_list = numbers.split()
+    num_list = [int(num) for num in num_list]
+    if function == "add":
+        print(sum(num_list))
+    elif function == "subtract":
+        print(num_list[0] - sum(num_list[1:]))
+    elif function == "multiply":
+        result = 1
+        for number in num_list:
+            result = result * number
+        print(result)
+    elif function == "divide":
+        result = num_list[0]
+        for number in num_list[1:]:
+            result = result / number
+        print(result)
+    else:
+        print("I don't understand that.")
