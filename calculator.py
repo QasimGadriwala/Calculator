9b456de89536ab7b9ce3ed13eaf398c9c14216f2
QasimGadriwala <qasimgadriwala@gmail.com>
2024-08-11 00:50:10 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,14 +4,19 @@ while True:
     2. Subtract
     3. Multiply
     4. Divide
-    5. Quit ''').lower()
+    5. Exponent
+    6. Quit ''').lower()
     if function == "quit":
         break
-    numbers = input("Please enter the numbers you would like to use separated by a space: ")
-    if numbers != "<class 'int>":
-       numbers = input("Please enter a number(s) seperated by a space ")
-    num_list = numbers.split()
-    num_list = [int(num) for num in num_list]
+    if function != "exponent":
+        while True:
+            numbers = input("Please enter the numbers you would like to use separated by a space: ")
+            num_list = numbers.split()
+            try:
+                num_list = [int(num) for num in num_list]
+                break
+            except ValueError:
+                print("Please enter valid numbers only.")
     if function == "add":
         print(sum(num_list))
     elif function == "subtract":
@@ -26,5 +31,14 @@ while True:
         for number in num_list[1:]:
             result = result / number
         print(result)
+    elif function == "exponent":
+        while True:
+            try:
+                base = int(input("What number would you like to use as the base? "))
+                exponent = int(input("What exponent would you like to use? "))
+                print(f"{base} to the power of {exponent} is {base ** exponent}")
+                break
+            except ValueError:
+                print("Please enter valid numbers only.")
     else:
         print("I don't understand that.")
